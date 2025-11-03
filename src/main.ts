4ab3f83076f6d6d7356802f875d4dfd2aee7f3ab
Janicke92 <142732047+Janicke92@users.noreply.github.com>
2025-11-03 19:11:26 +0100

    Setup for typescript, Vite and prettier.
    Setup for project with typescript and vite. Also added .prettier and made it work with typescript.

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import './style.css';
+
+function messyExample(name: string, age: number) {
+    console.log('Hi my name is ' + name + ' and I am ' + age + ' years old');
+}
+messyExample('Baelys', 23);
